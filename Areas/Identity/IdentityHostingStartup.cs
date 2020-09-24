7ccd2ce17fd9d7c28933bb35b09e7e2e490347eb
Mohamed Reda <68260013+Mohamed-Reda75@users.noreply.github.com>
2020-09-24 14:45:13 +0200

    Initial commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SecurityApp1.Areas.Identity.Data;
+using SecurityApp1.Data;
+
+[assembly: HostingStartup(typeof(SecurityApp1.Areas.Identity.IdentityHostingStartup))]
+namespace SecurityApp1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<SecurityDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SecurityDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => { options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireDigit = true;
+                
+                })
+                    .AddEntityFrameworkStores<SecurityDbContext>();
+            });
+        }
+    }
+}
